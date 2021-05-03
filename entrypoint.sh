dbdbb7ded8860d249411f3bdd47c8a9cd1de9948
Danil <zhulkin@gmail.com>
2021-05-03 12:44:29 +0300

    stage for deployment 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 cd server
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
 
-sudo python manage.py collectstatic --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
